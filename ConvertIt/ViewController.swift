978db615fb7dc5454c2580936d2038c43bdd2d8e
Carter Borchetta <carterborch@me.com>
2019-09-28 09:51:10 -0400

    6.4 Complete

--- ConvertIt/ViewController.swift
+++ ConvertIt/ViewController.swift
@@ -19,11 +19,38 @@ class ViewController: UIViewController {
     var formulaArray = ["miles to kilometers", "kilometers to miles", "feet to meters", "yards to meters", "meters to feet", "meters to yards"]
     var fromUnits = ""
     var toUnits = ""
+    var conversionString = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
         formulaPicker.delegate = self
         formulaPicker.dataSource = self
+    }
+    
+    func calculateConversion() {
+        var outputValue = 0.0
+        if let inputValue = Double(userInput.text!) {
+            switch conversionString {
+            case "miles to kilometers":
+                outputValue = inputValue / 0.62137
+            case "kilometers to miles":
+                outputValue = inputValue * 0.62137
+            case "feet to meters":
+                outputValue = inputValue / 3.2808
+            case "yards to meters":
+                outputValue = inputValue / 1.0936
+            case "meters to feet":
+                outputValue = inputValue * 3.2808
+            case "meters to yards":
+                outputValue = inputValue * 1.0936
+            default:
+                print("show alert - for some reason we didn't have a conversion string")
+            }
+            resultsLabel.text = "\(inputValue) \(fromUnits) = \(outputValue) \(toUnits)"
+        } else {
+            print("show alert here to show that the value was not a number")
+        }
+
     }
 
 
@@ -48,10 +75,12 @@ extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         // Whenever a row is clicked on this function is called
+        conversionString = formulaArray[row]
         let unitsArray = formulaArray[row].components(separatedBy: " to ") // This will contain the title(string) of the currently selected row and then we break it into a list
         fromUnits = unitsArray[0]
         toUnits = unitsArray[1]
         fromUnitsLabel.text = fromUnits
-        resultsLabel.text = toUnits
+        
+        calculateConversion()
     }
 }
